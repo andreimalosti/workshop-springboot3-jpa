1c5a76e6703a8e0653853d48a4b5a345759cd2f3
Andrei Malosti <andreipaulamalosti@yahoo.com>
2024-01-24 11:25:50 -0300

    Order, Instant, ISO 8601, part 2

---
+++ src/main/java/com/projetoandreijava/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.projetoandreijava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoandreijava.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
